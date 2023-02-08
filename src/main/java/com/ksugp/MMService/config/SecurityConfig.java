6576b94f671d4cc216a016374d516c9cbf7a89ca
Kirill Sehovich <kirill_sehovich@mail.ru>
2023-02-08 11:28:35 +0300

    + Permissions + CustomAuth

--- src/main/java/com/ksugp/MMService/config/SecurityConfig.java
+++ src/main/java/com/ksugp/MMService/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.ksugp.MMService.config;
 import com.ksugp.MMService.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,9 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,14 +24,28 @@ public class SecurityConfig{
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/").permitAll()
-                .requestMatchers(HttpMethod.GET,"/service/**").hasAnyRole(Role.ADMIN.name(),Role.USER.name())
-                .requestMatchers(HttpMethod.POST,"/service/**").hasRole(Role.ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE,"/service/**").hasRole(Role.ADMIN.name())
-                .requestMatchers(HttpMethod.PUT,"/service/**").hasRole(Role.ADMIN.name())
+//                .requestMatchers(HttpMethod.GET,"/service/**").hasAnyRole(Role.ADMIN.name(),Role.USER.name()) без пермишнов
+//                .requestMatchers(HttpMethod.POST,"/service/**").hasRole(Role.ADMIN.name())
+//                .requestMatchers(HttpMethod.DELETE,"/service/**").hasRole(Role.ADMIN.name())
+//                .requestMatchers(HttpMethod.PUT,"/service/**").hasRole(Role.ADMIN.name())
+//                .requestMatchers(HttpMethod.GET,"/service/**").hasAuthority(Permission.USERS_READ.getPermission()) без UserController/@PreAuthorize и SecurityConfig/@EnableMethodSecurity
+//                .requestMatchers(HttpMethod.POST,"/service/**").hasAuthority(Permission.USERS_WRITE.getPermission())
+//                .requestMatchers(HttpMethod.DELETE,"/service/**").hasAuthority(Permission.USERS_WRITE.getPermission())
+//                .requestMatchers(HttpMethod.PUT,"/service/**").hasAuthority(Permission.USERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/auth/login");
+
 
         return http.build();
     }
@@ -39,12 +55,14 @@ public class SecurityConfig{
                 User.builder().
                         username("admin").
                         password(passwordEncoder().encode("admin"))
-                        .roles(Role.ADMIN.name())
+//                        .roles(Role.ADMIN.name()) без пермишнов
+                        .authorities(Role.ADMIN.getAuthorities())
                         .build(),
                 User.builder().
                         username("user").
                         password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+//                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
--- src/main/java/com/ksugp/MMService/controller/UserController.java
+++ src/main/java/com/ksugp/MMService/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ksugp.MMService.controller;
 import com.ksugp.MMService.entity.User;
 import com.ksugp.MMService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,23 +19,33 @@ public class UserController {
     }
 
     @GetMapping("/show")
+    @PreAuthorize("hasAuthority('users:read')")
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
+
     @GetMapping("/show/{userId}")
+    @PreAuthorize("hasAuthority('users:read')")
     public Optional<User> getUser(@PathVariable Long userId){
         return userService.getUser(userId);
     }
+
     @PostMapping("/add")
+    @PreAuthorize("hasAuthority('users:write')")
     public void createUser(@RequestBody User user){
         userService.saveUser(user);
     }
+
     @DeleteMapping("/delete/{userId}")
+    @PreAuthorize("hasAuthority('users:write')")
     public void deleteUser(@PathVariable Long userId){
         userService.deleteUser(userId);
     }
+
     @PutMapping("/update/{userId}")
+    @PreAuthorize("hasAuthority('users:write')")
     public void updateUser(@RequestBody User user, @PathVariable Long userId){
         userService.updateUser(user,userId);
     }
 }
+//45 минута видео
